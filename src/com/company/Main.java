491ca0ba7e61245ad02b73f8fb60bbf599824510
lreubenstein <lreubenstein@billericak12.com>
2021-09-10 11:06:19 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+    
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("This is the beginning ");
+        System.out.println("Hello World");
+        System.out.println("Goodbye");
+    }
+}
